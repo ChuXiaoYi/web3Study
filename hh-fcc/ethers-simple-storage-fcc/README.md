0b1b8527e7f2e3e9afe25561b7c7b448d11c46de
ChuXiaoYi <895706056@qq.com>
2023-01-06 13:31:35 +0800

    javascript部署智能合约

---
+++ hh-fcc/ethers-simple-storage-fcc/README.md
@@ -0,0 +1,1 @@
+# Ethers Simple Storage FCC
---
+++ hh-fcc/ethers-simple-storage-fcc/cooking.js
@@ -0,0 +1,15 @@
+// setup movie night
+
+//cook popcorn
+//pour drinks
+//start movie
+
+async function setupMovieNight() {
+  await cookPopcorn();
+  await pourDrinks();
+  startMovie();
+}
+
+function cookPopcorn() {
+  return Promise();
+}
