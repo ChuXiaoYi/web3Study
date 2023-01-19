f18b7d20085f717f031ab386cf95a4c1e5adcf55
ChuXiaoYi <895706056@qq.com>
2023-01-19 21:38:49 +0800

    ✨: wave dapp

---
+++ buildspace/my-wave-portal/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+// Import and configure dotenv
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
